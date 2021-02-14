ad1e644a985195f68bb520cca6d99205f3e6af32
Carlos Bermudez <carlos.bs91@gmail.com>
2021-02-14 12:05:00 -0400

    Add axios dependence for api calls

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,11 @@ export default {
   buildModules: ["@nuxtjs/tailwindcss"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ["@nuxtjs/axios"],
+
+  axios: {
+    baseURL: "http://localhost"
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
